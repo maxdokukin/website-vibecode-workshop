6227e08b46834405ce6a96e0718afdc7e11426b6
maxdokukin <tools.shiny.01@icloud.com>
2025-09-12 12:47:37 -0700

    init website

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        name="Max Dokukin",
+        tagline="Student at SJSU",
+    )
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
